7a5e06cc56758e6c467f44090f22a622394b48c9
Reed <r.masek97@gmail.com>
2019-06-20 23:01:30 -0500

    Commented out code.

--- socrates/stepper.h
+++ socrates/stepper.h
@@ -1,3 +1,4 @@
+/*
 #include <Arduino.h>
 #define MOTOR_STEPS 200 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
 #define MICROSTEPS 1 // Microstepping mode. If you hardwired it to save pins, set to the same value here.
@@ -15,3 +16,4 @@ void stopStepper(){
 
 // may want to change some things - need to experiment with motor
 // implimenting sleep or disable may reduce energy used
+*/
