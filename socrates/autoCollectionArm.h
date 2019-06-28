56fb221f941f1d1827dade6f30158f0b14bf4cac
Reed <r.masek97@gmail.com>
2019-06-27 20:13:49 -0500

    Refined stepper motor logic. When shutting down, motor returns to starting position.

--- socrates/autoCollectionArm.h
+++ socrates/autoCollectionArm.h
@@ -5,67 +5,93 @@
 #include "include/StepperDriver/BasicStepperDriver.h"
 #include "include/StepperDriver/BasicStepperDriver.cpp"
 
-//#include "include/StepperDriver/DRV8834.h"
-//#include "include/StepperDriver/DRV8834.cpp"
-//#include "include/TimedAction/TimedAction.h"
-//#include "include/TimedAction/TimedAction.cpp"
-
 // Documentation for AccelStepper:
 // http://www.airspayce.com/mikem/arduino/AccelStepper/classAccelStepper.html
 
-//#define MOTOR_STEPS 200 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
-//#define MICROSTEPS 1 // Microstepping mode. If you hardwired it to save pins, set to the same value here.
-//#define DIR 7
-//#define STEP 8
-//#define M0 9
-//#define M1 10
-//DRV8834 stepper(MOTOR_STEPS, DIR, STEP, M0, M1);
-// ^^ may need above code... test current code and uncomment if not working
+// Code to use without AccelStepper library.
+/*
+  #include "include/StepperDriver/DRV8834.h"
+  #include "include/StepperDriver/DRV8834.cpp"
+  #include "include/TimedAction/TimedAction.h"
+  #include "include/TimedAction/TimedAction.cpp"
+  #define MOTOR_STEPS 200 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
+  #define MICROSTEPS 1 // Microstepping mode. If you hardwired it to save pins, set to the same value here.
+  #define DIR 10
+  #define STEP 11
+  #define M0 12
+  #define M1 13
+  DRV8834 stepper(MOTOR_STEPS, DIR, STEP, M0, M1);
+  // ^^ may need above code... test current code and uncomment if not working
+*/
+
 
 #define ACTUATOR_SIGNAL_PIN 6
 Servo actuator;
-AccelStepper stepper(AccelStepper::FULL4WIRE, 7, 8, 9, 10);
+AccelStepper stepper(AccelStepper::FULL4WIRE, 10, 11, 12, 13);
 boolean previousExtendBool;
 boolean extendBool = false;
-boolean actuatorState;
 
-void checkActuator(boolean current, boolean old){
-  if(old != current){
-    if(current == true){
+void checkActuator(boolean current, boolean old) {
+  if (old != current) {
+    if (current == true) {
       actuator.writeMicroseconds(2000);
-    }else{
+    } else {
       actuator.writeMicroseconds(1000);
     }
   }
 }
 
+// 1600 steps is a full revolution
 void spinStepperMotor() {
-  stepper.setMaxSpeed(500);
-  stepper.setAcceleration(50);
-  stepper.moveTo(360);
+  // Reset position to prevent integer overflow
+  if (stepper.currentPosition() >= 32000)
+  {
+    //Serial.println(stepper.currentPosition());
+    //Serial.println("Resetting position");
+    stepper.setCurrentPosition(stepper.currentPosition() - 32000);
+
+    // Need to reset max speed and acceleration when the position is reset
+    stepper.setMaxSpeed(1000);
+    stepper.setAcceleration(100);
+  }
+  stepper.moveTo(stepper.currentPosition() + 1600);
+
   //stepper.setSpeed(500);
-  // ^ may be needed - experiment 
+  // ^ may be needed - experiment
 }
 
 void stopStepperMotor() {
-  stepper.setAcceleration(-50);
-  stepper.moveTo(0);
-  stepper.setSpeed(0);
+  int currentPos = stepper.currentPosition();
+  int stepsInRev = currentPos % 1600;
+  int stepsToZero = 1600 - stepsInRev + 1600;
+  int finalPos = currentPos + stepsToZero;
+  stepper.moveTo(finalPos);
+  
+  // Debug prints
+  /*
+    Serial.print("stepsInRev: ");
+    Serial.println(stepsInRev);
+    Serial.print("stepsToZero: ");
+    Serial.println(stepsToZero);
+    Serial.print("finalPos: ");
+    Serial.println(finalPos);
+  */
 }
 
-
-
+// When checking pressure reading, leave a gap between the condition change
+// to prevent rapid switching on-off of the astrobio system during transition
+// between ascent/descent and float.
 void autoCollectionArm(float pressureReading) {
-   if (pressureReading < 0.5){
-     previousExtendBool = extendBool;
-     extendBool = true;
-     checkActuator(extendBool, previousExtendBool);
-     spinStepperMotor();
-     
-   }else if (pressureReading >= 0.5){
-     stopStepperMotor();
-     previousExtendBool = extendBool;
-     extendBool = false;
-     checkActuator(extendBool, previousExtendBool);
-   }
+  if (pressureReading < 0.5) {
+    previousExtendBool = extendBool;
+    extendBool = true;
+    checkActuator(extendBool, previousExtendBool);
+    spinStepperMotor();
+  } else if (pressureReading >= 1) {
+    if (extendBool)
+      stopStepperMotor();
+    previousExtendBool = extendBool;
+    extendBool = false;
+    checkActuator(extendBool, previousExtendBool);
+  }
 }
