3cb2e5dc9b17c196d598c4f400f9d8a405ffcf21
Reed <r.masek97@gmail.com>
2019-07-17 17:15:21 -0500

    Updated serial connection.

--- flight-computer/serialconnections/serialconnections.py
+++ flight-computer/serialconnections/serialconnections.py
@@ -16,12 +16,16 @@ remainingBytes = ''
 # Open connection with a specified port, return serial object
 def connectPort(portName):
     ser = None
+    connected = False
     try:
-        ser = serial.Serial(port = portName, baudrate = 4800, parity = serial.PARITY_NONE)
-        if ser != None:
-            logger.info('Successfully connected to port \'' + ser.name + '\' with baudrate ' + str(ser.baudrate) + '.')
-        else:
-            logger.warning('Could not connect to port ' + portName + '.')
+        for i,_ in enumerate(range(0, 3)):
+            if connected == False:
+                ser = serial.Serial(port = portName, baudrate = 4800, bytesize = serial.EIGHTBITS, parity = serial.PARITY_NONE, stopbits = serial.STOPBITS_ONE, xonxoff = False, rtscts = False)
+                if ser != None:
+                    logger.info('Successfully connected to port \'' + ser.name + '\' with baudrate ' + str(ser.baudrate) + '.')
+                    connected = True
+                else:
+                    logger.warning('Could not connect to port ' + portName + '. Attempt ' + str(i) + ' of 3.')
     except Exception as e:
         logger.error('Error while attempting to open serial port \'' + portName + '\'.')
         logger.error(e)
@@ -30,15 +34,15 @@ def connectPort(portName):
 
 def connectToHASP():
     logger.info('Attempting connection with HASP.')
-    usbPortName = ''
+    ttySPortName = ''
     dirList = os.listdir('/dev/')
     # Find all ttyUSB devices (i.e. connection via RS232)
     for name in dirList:
-        if 'USB' in name:
-            usbPortName = '/dev/' + name
-            logger.info('Found USB port: ' + str(usbPortName))
+        if 'ttyS' in name:
+            ttySPortName = '/dev/' + name
+            logger.info('Found ttyS port: ' + str(ttySPortName))
             break
-    return connectPort(usbPortName)
+    return connectPort(ttySPortName)
 
     
 def connectToArduino():
@@ -84,8 +88,14 @@ def packetHandler(arduino_serial_connection):
                 logger.debug('Complete packet: ' + completePacket)
                 packetComplete = True
                 remainingBytes = b''
+                remainingCommas =  completePacket.count(',') % 15
+                while remainingCommas is not 0:
+                    completePacket = completePacket[:completePacket.find(',')]
+                    print(completePacket)
+                    remainingCommas =  completePacket.count(',') % 15
                 return completePacket
         
 def downlinkPacket(hasp_serial_connection, dataPacket):
-    numBytesWritten = hasp_serial_connection.write(bytes(dataPacket))
-    logger.info('Sent ['+str(numBytesWritten)+'] bytes to HASP: '+str(dataPacket.rstrip()))
+    #numBytesWritten = hasp_serial_connection.write(bytes(dataPacket))
+    hasp_serial_connection.write(bytes(dataPacket))
+    logger.info('Sent bytes to HASP: '+str(dataPacket.rstrip()))
